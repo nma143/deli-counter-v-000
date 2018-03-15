213cdcf50e83b30abdb28164154dd40099f1084c
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-15 16:18:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,8 +12,9 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
+  katz_deli << name
   position = katz_deli.length + 1
-  return [name, position]
+  return "Welcome, #{name}. You are number #{position} in line."
 end
 
 def now_serving (katz_deli)
