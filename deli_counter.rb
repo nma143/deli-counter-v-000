8145324c1f630cc100b068037a0b5ffcb3aa0ee5
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-15 16:11:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def line
+  
+end
+
+def take_a_number(katz_deli, name)
+  position = katz_deli.length + 1
+  return [name, position]
+end
+
+def now_serving (katz_deli)
+
+  if(katz_deli.length==0)
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}"
+    katz_deli.shift
+  end
+
+end
