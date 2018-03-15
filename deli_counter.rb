384db694be8f55622d37a2b2b1e3b32be8dad5f9
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-15 16:20:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  position = katz_deli.length + 1
+  position = katz_deli.length
   puts "Welcome, #{name}. You are number #{position} in line."
 end
 
